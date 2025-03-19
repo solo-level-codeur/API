33b5339065d0ac1a8d2c1e2c032fb869c9b0fafc
Hessaine Abdelkarim <hessaineabdelkarim@gmail.com>
2025-03-19 12:08:31 +0100

    mise a jour 3

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
     host: process.env.MYSQLHOST,  // L'hôte MySQL depuis les variables d'environnement Railway
     user: process.env.MYSQLUSER,  // L'utilisateur MySQL
     password: process.env.MYSQLPASSWORD,  // Le mot de passe de l'utilisateur MySQL
-    database: process.env.MYSQL_DATABASE,  // Nom de la base de données
+    database: process.env.MYSQLDATABASE,  // Nom de la base de données
     port: process.env.MYSQLPORT || 3306,  // Le port de la base de données (3306 est le port par défaut pour MySQL)
 }).promise();  // Active l'utilisation des promesses avec async/await
 
